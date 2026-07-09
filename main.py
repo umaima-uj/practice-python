1a4db7af95389e22357c3abf24b92c850ea0f44b
umaima-uj <umaimaurooj18@gmail.com>
2026-07-09 14:12:49 +0500

    data types

---
+++ main.py
@@ -0,0 +1,17 @@
+
+a = "umaima" 
+b = 21
+c = True 
+d = None
+e = 3.5
+print ("data type of a is" ,type(a))
+print ("data type of b is" ,type(b))
+print ("data type of c is" ,type(c))
+print ("data type of d is" ,type(d))
+print ("data type of e is" ,type(e))
+
+
+
+
+
+
