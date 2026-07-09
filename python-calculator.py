9354ae4b7381d89c6a2d99a215ae82ecd7bfabdc
umaima-uj <umaimaurooj18@gmail.com>
2026-07-09 16:18:44 +0500

    first commit

---
+++ python-calculator.py
@@ -0,0 +1,22 @@
+print ("1  addition")
+print ("2  subtraction")
+print ("3 multiplication")
+print ("4  division"2)
+operation = int(input("please select an operation:"))
+if(operation in [1,2,3,4]):
+     num1 = float(input("choose first number:"))
+     num2 = float(input("choose second number:"))
+
+     if (operation ==1):
+         result = num1 + num2  
+     elif (operation ==2):
+         result =num1 -num2
+     elif (operation ==3):
+         result = num1 * num2
+     elif (operation ==4):
+         result =num1/num2
+         
+     else:
+         print("invalid operation")
+         
+     print("the result of operation is {}:" .format(result))
