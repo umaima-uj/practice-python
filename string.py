71665c65b137f8ab16f81e088c8b4b7e483dfa9c
umaima-uj <umaimaurooj18@gmail.com>
2026-07-10 17:10:19 +0500

    sting slicing

---
+++ string.py
@@ -0,0 +1,14 @@
+a = "umaima"
+b = "tayyaba"
+#simple slicing
+print (a[0:6])
+print (b[:7])
+# negative slicing
+print (a[-6:-1])
+print (b[-7:-1])
+#slicing with skipping items
+print(a[0:6:2])
+print(b[0:7:1])
+#reverse slicing
+print(a[::-1])
+print(b[::-2])
