802d2b830d450ba896eec7cc78c45b0b1e6f5b25
umaima-uj <umaimaurooj18@gmail.com>
2026-07-12 19:08:16 +0500

    match statement

---
+++ match_case.py
@@ -0,0 +1,11 @@
+x =int(input("enter the number: "))
+match x:
+    case 9:
+        print("the number is nine")
+    case 10:
+        print("the number is ten")
+    case _:
+        print("the number is" ,x)
+
+         
+        
