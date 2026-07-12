7ac4e5569b24bb74b8f235cf4850c547ae2d0ca3
umaima-uj <umaimaurooj18@gmail.com>
2026-07-12 21:14:05 +0500

    for loop in python

---
+++ day_17.py
@@ -0,0 +1,12 @@
+name = "umaima urooj"
+for i in name:
+    print(i)
+for k in range(5):
+    print(k)
+print()
+for j in range(1,6):
+    print(j+1)
+print()
+for l in range(1,12,2):  
+    print(l)
+print()
