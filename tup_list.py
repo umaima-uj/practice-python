a1f6f115f82e0d456c14a185b8ec88ed7b027392
umaima-uj <umaimaurooj18@gmail.com>
2026-07-13 16:52:51 +0500

    tuple and list #lecture 23 and 24

---
+++ tup_list.py
@@ -0,0 +1,18 @@
+list =[3,7, 5.5, 9, 7, 98, 75]
+print (list)
+list.append (100)
+print (list)
+list.sort()
+print(list)
+list.reverse()
+print(list)
+print (list.count(6))
+print (list.index(7))
+m=list
+m[0]=18
+print(m)
+k = list+m
+print(k)
+tuple =("umaima", 21, "tayyaba",25, "farhan", 20)
+print(tuple)
+print(tuple[1:6])
