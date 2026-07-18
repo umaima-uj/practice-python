4e10e281b0eb27dc06e0c28b81e35d14f5fcdd74
umaima-uj <umaimaurooj18@gmail.com>
2026-07-18 22:15:37 +0500

    from lecture 42

---
+++ enume_rate.py
@@ -0,0 +1,3 @@
+students =["umaima","farhan","tahira","amir","anila","usama","kamran","seerat","tayyaba","asad"]
+for roll_number,student in enumerate(students,start=1):
+    print(f"Roll number {roll_number}:{student}")
